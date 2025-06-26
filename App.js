1a24f98b4cb19961688061b49a753a09e6dceddd
bolollo <bolosig@gmail.com>
2025-06-26 16:02:28 +0200

    CU-1258 change rnmapbox with maplibre lib

--- App.js
+++ App.js
@@ -1,46 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-import MapLibreGL from '@rnmapbox/maps';
+import { MapView, Camera } from "@maplibre/maplibre-react-native";
 
 export default function App() {
-  MapLibreGL.setWellKnownTileServer(MapLibreGL.TileServers.MapLibre);
-  MapLibreGL.setAccessToken(null);
 
   const MAPTILER_API_KEY = "YOUR_MAPTILER_API_KEY_HERE";
 
   return (
-    <View style={styles.page}>
-      <View style={styles.container}>
-        <MapLibreGL.MapView
-          style={styles.map}
-          styleURL={`https://api.maptiler.com/maps/streets-v2/style.json?key=${MAPTILER_API_KEY}`}
+    <View style={styles.container}>
+      <View style={styles.mapcontainer}>
+        <MapView 
+          style={styles.map} 
+          mapStyle={`https://api.maptiler.com/maps/streets-v2/style.json?key=${MAPTILER_API_KEY}`}
           logoEnabled={false}
           attributionPosition={{bottom: 8, right: 8}}>
-          <MapLibreGL.Camera
-            defaultSettings={{centerCoordinate: [2, 41.5], zoomLevel: 8}}
-          />
-        </MapLibreGL.MapView>
+            <Camera
+              centerCoordinate={[2, 41.5]}
+              zoomLevel={8}
+              animationDuration={2000}
+              animationMode="easeTo"
+            />
+        </MapView>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapcontainer: {
     width: '100%',
     height: '100%',
   },
   map: {
     flex: 1,
-  },
-  image: {
-    width: 25,
-    height: 25,
   },
 });
